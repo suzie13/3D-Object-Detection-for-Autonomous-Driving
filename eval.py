e3de0ee15503b8b193ed2ec93bdb4cc2c587730d
suzie13 <sush.schandra@gmail.com>
2023-02-15 13:36:41 -0600

    modifications

--- eval.py
+++ eval.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import time
 import cv2
+# from scipy import misc
 import imageio
 
 from utils import *
@@ -20,20 +21,22 @@ def drawRect(img, pt1, pt2, pt3, pt4, color, lineWidth):
     cv2.line(img, pt1, pt4, color, lineWidth)
 
 
-def get_region_boxes(x, conf_thresh, num_classes, anchors):
+def get_region_boxes(x, conf_thresh, num_classes, anchors, num_anchors):
     if x.dim() == 3:
         x = x.unsqueeze(0)
 
-    assert (x.size(1) == (7 + num_classes) * 5)
+    assert (x.size(1) == (7 + num_classes) * num_anchors)
 
-    nA = 5  # num_anchors = 5
+    nA = num_anchors  # num_anchors = 5
     nB = x.data.size(0)
     nC = num_classes  # num_classes = 8
     nH = x.data.size(2)  # nH  16
     nW = x.data.size(3)  # nW  32
 
+    # Tensors for cuda support
     FloatTensor = torch.cuda.FloatTensor if x.is_cuda else torch.FloatTensor
-
+    LongTensor = torch.cuda.LongTensor if x.is_cuda else torch.LongTensor
+    ByteTensor = torch.cuda.ByteTensor if x.is_cuda else torch.ByteTensor
 
     prediction = x.view(nB, nA, 7+num_classes, nH, nW).permute(0, 1, 3, 4, 2).contiguous()
 
@@ -62,17 +65,14 @@ def get_region_boxes(x, conf_thresh, num_classes, anchors):
     pred_boxes[..., 6] = pred_conf
     pred_boxes[..., 7:(7 + nC) ] = pred_cls
 
-    pred_boxes = pred_boxes.transpose(0, 1).contiguous().view(-1, (7 + nC)) # torch.Size([2560, 15])
-    pred_boxes = torch.FloatTensor(pred_boxes.size()).copy_(pred_boxes)
+    pred_boxes = convert2cpu(pred_boxes.transpose(0, 1).contiguous().view(-1, (7 + nC)))  # torch.Size([2560, 15])
 
     all_boxes = []
     for i in range(2560):
-        if i < pred_boxes.shape[0]:
-            if pred_boxes[i][6] > conf_thresh:
-                all_boxes.append(pred_boxes[i])
-                # print(pred_boxes[i])
+        if pred_boxes[i][6] > conf_thresh:
+            all_boxes.append(pred_boxes[i])
+            # print(pred_boxes[i])
     return all_boxes
-
 
 
 
@@ -84,7 +84,7 @@ bc['maxY'] = 40
 bc['minZ'] = -2
 bc['maxZ'] = 1.25
 
-for file_i in range(280, 299):
+for file_i in range(266, 269):
     test_i = str(file_i).zfill(6)
 
     lidar_file = 'D:/3D-Object-Detection-for-Autonomous-Driving/dataset/kitti/training/velodyne/' + test_i + '.bin'
@@ -92,29 +92,39 @@ for file_i in range(280, 299):
     label_file = 'D:/3D-Object-Detection-for-Autonomous-Driving/dataset/kitti/training/label_2/' + test_i + '.txt'
 
     calib = load_kitti_calib(calib_file)
-    target = get_target(label_file, calib['Tr_velo2cam'])
+    # target = get_target(label_file, calib['Tr_velo2cam'])
+    target = get_target2(label_file)
+    # print(target)
 
     # load point cloud data
     a = np.fromfile(lidar_file, dtype=np.float32).reshape(-1, 4)
+    #print("a=",a.shape)#a= (116908, 4)
     b = removePoints(a, bc)
-    rgb_map = makeBVFeature(b, 40 / 512)
+    # print("b=",b.shape)#b= (60123, 4)
+    rgb_map = makeBVFeature(b, bc, 40 / 512)
+    # print("rgb_map=",rgb_map.shape)# (512, 1024, 3)
     imageio.imsave('eval_bv.png', rgb_map)
 
-
+    
     input = torch.from_numpy(rgb_map)  # (512, 1024, 3)
     input = input.reshape(1, 3, 512, 1024)
-    # model = torch.load('epoch_151.pth')
-    # model = torch.load('CY_epoch3')
-    model = torch.load('newww_CY_epoch100')
+    # model = torch.load('ComplexYOLO_epoch100')
+    # model = torch.load('epoch_150.pth')
+    # model = torch.load('newww_CY_epoch100')
+    # model = torch.load('ComplexYOLO_epoch198.pth')
+    model = torch.load('old100')
+    
     model.cuda()
     output = model(input.float().cuda())  # torch.Size([1, 75, 16, 32])
 
-
+    # eval result
     conf_thresh = 0.7
-    classes = int(8)
+    nms_thresh = 0.4
+    num_classes = int(8)
+    num_anchors = int(5)
     img = cv2.imread('eval_bv.png')
 
-    all_boxes = get_region_boxes(output, conf_thresh, classes, anchors)
+    all_boxes = get_region_boxes(output, conf_thresh, num_classes, anchors, num_anchors)
 
     for i in range(len(all_boxes)):
         pred_img_y = int(all_boxes[i][0] * 1024.0 / 32.0)  # 32 cell = 1024 pixels
@@ -127,6 +137,21 @@ for file_i in range(280, 299):
         rect_bottom1 = int(pred_img_y + pred_img_width / 2)
         rect_bottom2 = int(pred_img_x + pred_img_height / 2)
         cv2.rectangle(img, (rect_top1, rect_top2), (rect_bottom1, rect_bottom2), (255, 0, 0), 1)
+        # print("Class=",all_boxes[i][8])
+        cv2.rectangle(img, (rect_top1, rect_top2), (rect_bottom1, rect_bottom2), (255, 0, 0), 1)
+        # class_index = int(all_boxes[i][7])
+        # class_name = class_list[class_index]
+        class_probabilities = all_boxes[i][7:(7 + 7)]
+        print(class_probabilities.shape)
+        # class_probabilities = # a PyTorch tensor
+        class_index = torch.argmax(class_probabilities)
+        class_index = class_index.detach().numpy()
+        # class_index = np.argmax(class_probabilities)
+        class_name = class_list[class_index]
+        # rect_top2 += 20
+        cv2.putText(img, class_name, (rect_top1, rect_top2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
 
-    imageio.imsave('resultant_img' + test_i + '.png', img)
+        # cv2.putText(img, "car", (rect_top1, rect_top2), cv2.FONT_HERSHEY_SIMPLEX, 1,(255, 0, 0))
+
+    imageio.imsave('new_detection_old_weight_redoanchors' + test_i + '.png', img)
 
